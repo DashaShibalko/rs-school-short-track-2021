880b85e81cb8352b2763f392f8a729e0e23ba028
DariaShibalko <daryaavramo@yandex.by>
2021-04-12 17:20:26 +0300

    feat: done 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const s = str.split('');
+  let k;
+  for (let i = 0; i < s.length; i++) {
+    k = i + 1;
+    while (s[i] === s[k]) {
+      k++;
+    }
+    if ((k - i) > 1) {
+      s.splice(i, 0, (k - i));
+      s.splice(i + 2, k - i - 1);
+    }
+  }
+  return s.join('');
 }
 
 module.exports = encodeLine;
